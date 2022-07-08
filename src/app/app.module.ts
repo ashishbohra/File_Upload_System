5efc47219951352e209a91d110fe8d0215be0e99
ashishbohra <ashishbohra01@gmail.com>
2022-07-08 19:02:36 +0530

    changes for set preference model creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './Home/app.component';
-import { FileUploadComponent } from './file-upload/file-upload.component';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon'
 import {MatNativeDateModule} from '@angular/material/core';
 import { MatTableModule } from '@angular/material/table';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import { HttpClientModule } from '@angular/common/http';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatInputModule} from '@angular/material/input';
+
+import { FileUploadComponent } from './file-upload/file-upload.component';
 import { SharePreferencesDetailsComponent } from './share-preferences-details/share-preferences-details.component';
 
 
 const MaterialComponents=[
-  MatButtonModule,MatIconModule,MatNativeDateModule,MatProgressBarModule
+  MatButtonModule,MatIconModule,MatNativeDateModule,MatProgressBarModule,
+  MatRadioModule,MatSlideToggleModule,MatInputModule
 ]
 
 @NgModule({
@@ -26,6 +32,7 @@ const MaterialComponents=[
     SharePreferencesDetailsComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     MaterialComponents,
--- src/app/share-preferences-details/share-preferences-details.component.ts
+++ src/app/share-preferences-details/share-preferences-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
 
 export interface fileData {
   name: string;
@@ -7,6 +9,15 @@ export interface fileData {
 const ELEMENT_DATA: fileData[] = [
 ];
 
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+
 @Component({
   selector: 'app-share-preferences-details',
   templateUrl: './share-preferences-details.component.html',
@@ -14,6 +25,14 @@ const ELEMENT_DATA: fileData[] = [
 })
 export class SharePreferencesDetailsComponent implements OnInit {
   @Input() dataSource:fileData[] ;
+
+  favoritepreference: string;
+  preferences: string[] = ['Share Via mail', 'Get Link'];
+
+
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  matcher = new MyErrorStateMatcher();
 
   displayedColumns: string[] = [ 'name'];
   constructor() { }
